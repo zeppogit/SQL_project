85a8338e313943bf11e9c76203c90cad835a89e3
zeppogit <zeppocode@gmail.com>
2021-11-04 20:38:40 -0400

    to add TradeLog and StocksHeld tables, and changing foreign key names to match primary key names (instead of using underscores for foreign keys)

--- StockBroker-CreateDBObjects.sql
+++ StockBroker-CreateDBObjects.sql
@@ -15,15 +15,24 @@
     Tables
 
 **********************************************************/
+IF OBJECT_ID('TradeLog', 'U') IS NOT NULL
+    DROP TABLE TradeLog;
 
-IF OBJECT_ID('Clients', 'U') IS NOT NULL 
-    DROP TABLE Clients;
-    
+
+IF OBJECT_ID('StocksHeld', 'U') IS NOT NULL
+    DROP TABLE StocksHeld;   
+
 IF OBJECT_ID('Portfolios', 'U') IS NOT NULL
     DROP TABLE Portfolios;
 
+IF OBJECT_ID('Clients', 'U') IS NOT NULL 
+    DROP TABLE Clients;
+
+
 IF OBJECT_ID('Stocks', 'U') IS NOT NULL
-    DROP TABLE Stocks;
+    DROP TABLE Stocks;   
+
+
 
 CREATE TABLE Clients
 (
@@ -41,39 +50,58 @@ GO
 CREATE TABLE Portfolios
 (
     [PortfolioID] INT NOT NULL IDENTITY PRIMARY KEY,
-    [Client_id] INT NOT NULL, 
+    [ClientID] INT NOT NULL, 
     [Type] NVARCHAR(50) NOT NULL,
             -- Type = Regular, IRA, Roth, InheritedIRA, InheritedRoth, Trust
-    FOREIGN KEY(Client_id) REFERENCES Clients(ClientID)
+    FOREIGN KEY(ClientID) REFERENCES Clients(ClientID)
 );
 GO
 
---Stocks (StockID, StockName, StockExchange, ClosingPrice, PE) --can i use / in a name?
+
 
 CREATE TABLE Stocks
 (
-    [StocksID] INT NOT NULL IDENTITY PRIMARY KEY,
+    [StockID] INT NOT NULL IDENTITY PRIMARY KEY,
+    [Symbol] NVARCHAR(10) NOT NULL,  
     [StockName] NVARCHAR(50) NOT NULL,
     [StockExchange] NVARCHAR(10) NOT NULL,
-    [ClosingPrice] INT NOT NULL, -- need type for dollars
+    [ClosingPrice] FLOAT NOT NULL, -- need type for dollars
     [PE] INT NOT NULL,
 );
 GO
 
-/*
+
 CREATE TABLE StocksHeld
 (
-    [StocksID] INT NOT NULL IDENTITY PRIMARY KEY,
-    [Portfolio_id] INT NOT NULL, 
+    [StocksHeldID] INT NOT NULL IDENTITY PRIMARY KEY,
+    [StockID] INT NOT NULL,
+    [PortfolioID] INT NOT NULL, 
     [NumShares] INT NOT NULL,
-    FOREIGN KEY(Portfolio_id) REFERENCES Portfolios(PortfolioID)
+    FOREIGN KEY(PortfolioID) REFERENCES Portfolios(PortfolioID),
+    FOREIGN KEY(StockID) REFERENCES Stocks(StockID)
 );
 GO
-*/
+
+-- TradeLog (TradeID, datetime, Portfolio_id, Buy/Sell/TransferIn/TransferOut, Stocks_id, Num, Price)
+
+CREATE TABLE TradeLog
+(
+    [TradeID] INT NOT NULL IDENTITY PRIMARY KEY,
+    [DateTime] DATETIME NOT NULL,
+    [PortfolioID] INT NOT NULL, 
+    [BuySellInOut] INT NOT NULL,
+    [StockID] INT NOT NULL,
+    [Num] INT NOT NULL,
+    [Price] FLOAT NOT NULL, -- need type for dollars
+    FOREIGN KEY(PortfolioID) REFERENCES Portfolios(PortfolioID),
+    FOREIGN KEY(StockID) REFERENCES Stocks(StockID)
+);
+GO
+
 
 /** TEMP HERE FOR CONVENIENCE -  SEE other file  LoadData - testing here */
 
 --MY VERIFICATION
 SELECT *
-FROM Stocks
+FROM TradeLog
 GO
