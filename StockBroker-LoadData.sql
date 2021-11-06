21d22bf5884640adf1bd5137a5b0d00ab3ad7915
zeppogit <zeppocode@gmail.com>
2021-11-06 17:50:35 -0400

    to add data to be loaded  into Stocks table and Clients table

--- StockBroker-LoadData.sql
+++ StockBroker-LoadData.sql
@@ -6,31 +6,78 @@
     Portfolios (PortfolioID, Client_id, Type)  
         -- Type = Regular, IRA, Roth, InheritedIRA, InheritedRoth, Trust)
     Stocks (StockID, StockName, StockExchange, ClosingPrice, PE) --can i use / in a name?
-    StocksHeld (Stock_id, Portfolio_id, NumShares)  --or might call it Holdings
+    StocksHeld (Stock_id, Portfolio_id, NumShares) 
     TradeLog (TradeID, datetime, Portfolio_id, Buy/Sell/TransferIn/TransferOut, Stock_id, Num, Price)
  */
  
-INSERT INTO Clients
-VALUES ('John', 'Smith','101 Elm St, Muncie, IN, 47306','765-289-5555','1980-10-01', '2021-10-12 11:10:00'),
-       ('Jane', 'Doe', '200 Main St, Muncie, IN, 47306', '765-555-3232', '1975-09-24', '2021-09-16 13:30:00'),
-       ('Ed', 'Norton', '300 Sewer St, Muncie, IN, 47306', '765-555-3600', '1927-03-14', '2021-03-20 15:40:00');
+INSERT INTO Clients --- WILL PROBABLY DELETE LAST CONTACTED FIELD
+VALUES  ('John', 'Smith','101 Elm St, Muncie, IN, 47306','765-289-5555','1980-10-01', '2021-10-12 11:10:00'),
+        ('Jane', 'Doe', '200 Main St, Muncie, IN, 47306', '765-555-3232', '1975-09-24', '2021-09-16 13:30:00'),
+        ('Henry', 'Miller', '300 10th St, Muncie, IN, 47306', '765-556-3442', '1985-03-24', '2021-09-16 10:30:00'),
+        ('Alice', 'Cramden', '200 Main St, Muncie, IN, 47306', '765-555-3552', '1935-01-12', '2021-11-16 15:30:00'),
+        ('Ed', 'Norton', '300 Sewer St, Muncie, IN, 47306', '765-555-3600', '1927-03-14', '2021-03-20 15:40:00');
 GO
 
-INSERT INTO Portfolios
+INSERT INTO Portfolios      -- Type = Regular, IRA, Roth, InheritedIRA, InheritedRoth, Trust)
 VALUES (1, 'Roth'),
         (1, 'Regular'),
         (2, 'Regular'),
         (3, 'IRA'),
-        (3, 'Regular');
+        (3, 'Regular'),
+        (4, 'Regular'),
+        (4, 'InheritedIRA'),
+        (5, 'Regular'),
+        (5, 'IRA');
 GO
 
 
---Stocks (StockID, StockName, StockExchange, ClosingPrice, PE) --can i use / in a name?
+--Stocks (StockID, StockName, StockExchange, ClosingPrice, PE) 
 INSERT INTO Stocks
-VALUES ('GOOG', 'Alphabet', 'NASD', 2875.48, 27);
-GO
+VALUES ('GOOG', 'Alphabet Inc', 'NASDAQ', 2875.48, 27),
+    ('F', 'Ford Motor Company', 'NASDAQ', 19.20, 27),
+    ('DIS', 'Walt Disney Company (The)', 'NASDAQ', 175.63, 280),
+    ('SNOW', 'Snowflake Inc', 'NASDAQ', 364.80, NULL),
+    ('WMT', 'Walmart Inc.', 'NASDAQ', 152.23, 42),
+    ('GS', 'Goldman Sachs Group (The), Inc', 'NASDAQ', 407.08, 7),
+    ('HD', 'Home Depot, Inc', 'NYSE', 368.40, 26),
+    ('HON', 'Honeywell International, Inc', 'NASDAQ', 226.06, 29),
+    ('AMZN', 'Amazon.com, Inc', 'NASDAQ', 3517.99, 69),
+    ('TSLA', 'Tesla, Inc.', 'NASDAQ', 1222.09, 396),
+    ('VZ', 'Verizon Communications Inc', 'NYSE', 52.24, 10),
+    ('SHOP', 'Shopify, Inc', 'NYSE', 1528.06, 57),
+    ('CRM', 'Salesforce.com, Inc', 'NYSE', 307.25, 122),
+    ('NVDA', 'NVIDIA Corporation', 'NASDAQ', 297.29, 106),
+    ('TMUS', 'T-Mobile US, Inc', 'NASDAQ', 122.27, 46),
+    ('ADBE', 'Adobe Inc.', 'NASDAQ', 661.90, 55),
+    ('XPO', 'XPO Logistics, Inc.', 'NYSE', 78.60, 24),
+    ('GM', 'General Motors Company', 'NYSE', 58.52, 8),
+    ('KO', 'Coca-Cola Company (The)', 'NYSE',56.84, 28),
+    ('LLY', 'Eli Lilly and Company', 'NYSE', 265, 40),
+    ('LOW', 'Lowes Companies, Inc', 'NYSE', 235.54, 25),
+    ('LVS', 'Las Vegas Sands Corp.', 'NYSE', 42.60, NULL),
+    ('MCD', 'McDonalds Corp.', 'NYSE', 254.55, 26),
+    ('MELI', 'MercadoLibre, Inc', 'NYSE', 1629.76, 1024),
+    ('MRNA', 'Moderna, Inc', 'NASDAQ', 236.99, 14),
+    ('NKE', 'Nike, Inc', 'NYSE', 177.51, 59),
+    ('MU', 'Micron Technology, Inc', 'NASDAQ', 72.92, 14),
+    ('NFLX', 'Netflix, Inc', 'NASDAQ', 645.52, 59),
+    ('NOW', 'ServiceNow, Inc', 'NYSE', 692.01, 634),
+    ('PEP', 'PepsiCo, Inc', 'NASDAQ', 165.51, 28),
+    ('TGT', 'Target Corp.', 'NYSE', 254.27, 20),
+    ('VIAC', 'ViacomCBS, Inc', 'NASDAQ', 36.34, 7),
+    ('NOK', 'Nokia Corp.', 'NYSE', 5.77, NULL);
+    GO
 
---MY VERIFICATION
+    --  StocksHeld (Stock_id, Portfolio_id, NumShares) 
+/*
+INSERT INTO StocksHeld   
+VALUES (
+    };
+GO
+*/
+
+
+--TEMPORARY FOR MY VERIFICATION
 SELECT *
 FROM Portfolios
 GO
@@ -41,4 +88,13 @@ GO
 
 SELECT *
 FROM Clients
+GO
+
+
+SELECT *
+FROM StocksHeld
+GO
+
+SELECT *
+FROM TradeLog
 GO
